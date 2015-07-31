656648deff91e8d44c513492716053440856a6c4
Koding <martinema@yahoo.com>
2015-07-31 09:08:19 +0000

    Fin tema7. Comienzo prÃctica obligatoria

--- views/quizes/show.js
+++
@@ -1,5 +0,0 @@
-<form method ="get" action="/quizes/<%= quiz.id %>/answer">
-	Pregunta: <%= quiz.pregunta %> <br/>
-	<input type="text" name="respuesta" value="Respuesta"/>
-	<input type="submit" name="Enviar />
-</form>
