44e92e6bff49743ebe22b9dca95974318e65ad65
Koding <martinema@yahoo.com>
2015-08-03 10:56:24 +0000

    Quiz 15

--- models/models.js
+++ models/models.js
@@ -27,8 +27,18 @@ var sequelize = new Sequelize (DB_name, user, pwd,
 				);
 
 //Importar la definición de la tabla Quiz en quiz.js
-var Quiz = sequelize.import (path.join(__dirname, 'quiz'));
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import (quiz_path);
+
+//Importar la defición de la tabla Comment
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
+
 exports.Quiz = Quiz; //exportar definición de la tabla Quiz
+exports.Comment = Comment;
 
 //sequelize.sync() crea e inicializa tabla de preguntas en BD
 sequelize.sync().then(function (){
